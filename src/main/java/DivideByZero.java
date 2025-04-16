95c462634eed66569501295f815d47d18781cbc3
Akshay krishna <54903431+ABK4793@users.noreply.github.com>
2025-04-16 23:48:37 +0530

    Add files via upload

---
+++ src/main/java/DivideByZero.java
@@ -0,0 +1,12 @@
+// https://sig-product-docs.synopsys.com/bundle/coverity-docs/page/checker-ref/checkers/D/divide_by_zero.html
+
+class DivideByZero {
+    void testDiv(int a, int b)
+    {
+        if (a!=0) {
+           //Do something
+        }
+        int y = b / a;
+    }
+}
+// creating stash branch
